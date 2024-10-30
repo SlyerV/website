dde815c3dad4c9bf0be7e99c60611974a5b63f32
SlyerV <80004341+SlyerV@users.noreply.github.com>
2024-10-29 21:06:44 -0700

    Update script.js

--- clickr/script.js
+++ clickr/script.js
@@ -90,15 +90,16 @@ function increase() {
   update()
 }
 function reset() {
-  confirm("Are you sure you want to reset all of your progress?")
-  clicks = 0
-  mIncr = 1
-  mCost = 20
-  aCost = 100
-  aIncr = 0
-  mLevel = 0
-  aLevel = 0
-  update()
+  if (confirm("Are you sure you want to reset all of your progress?")) {
+      clicks = 0
+      mIncr = 1
+      mCost = 20
+      aCost = 100
+      aIncr = 0
+      mLevel = 0
+      aLevel = 0
+      update()
+  }
 }
 function manbuy() {
   if ((clicks > (mCost-1)) && (mIncrs[mLevel] != "MAX")) {
